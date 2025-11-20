5795685ba26607988fd3304bd10203836ba9c839
Terraform User <builder-admin@example.com>
2025-11-20 15:47:01 +0000

    Managed by Terraform

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "openstack_networking_router_interface_v2" "router_interface_1" {
   subnet_id = openstack_networking_subnet_v2.subnet1.id
 }
 
-resource "openstack_networking_router_interface_v2" "router_interface_1" {
+resource "openstack_networking_router_interface_v2" "router_interface_2" {
   router_id = openstack_networking_router_v2.router_1.id
   subnet_id = openstack_networking_subnet_v2.subnet2.id
 }
